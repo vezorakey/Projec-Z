773a65c0f8c1fe1dad6bc41d434a1453d122cc5e
vzk onty <vezorakey@gmail.com>
2024-11-26 18:34:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Menambahkan event ke tombol navbar
+document.getElementById("homeButton").addEventListener("click", function () {
+  alert("Naik Anjing AEE!");
+});
+
+document.getElementById("serverButton").addEventListener("click", function () {
+  alert("Setan!");
+});
+
+document.getElementById("storeButton").addEventListener("click", function () {
+  alert("VEZORAKEY ganteng😘");
+});
+
+document.getElementById("contactButton").addEventListener("click", function () {
+  alert("Bajingan!");
+});
+
+// Tombol Subscribe
+document
+  .querySelector(".subscribe-button")
+  .addEventListener("click", function () {
+    alert("Terima kasih telah MENGENTOD!");
+  });
